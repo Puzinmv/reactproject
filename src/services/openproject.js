4ac8ed2294ae2d5edbe0e5a1d76a3c4222e0b1c7
Пузин Михаил <puzinmv@gmail.com>
2024-09-09 23:14:20 +0500

    try 4

--- src/services/openproject.js
+++ src/services/openproject.js
@@ -122,13 +122,16 @@ export const CreateProject = async (formData) => {
         };
 
         const response = await axios.request(config)
+        console.log(response);
         const location = response.headers?.location;
+        console.log(location);
         if (!location) {
             throw new Error('Не удалось получить Location из ответа');
         }
 
         let retries = 0;
         while (retries < MAX_RETRIES) {
+            console.log(retries);
             const jobStatusResponse = await axios.get(location, {
                 headers: {
                     'Authorization': 'Basic ' + API_KEY

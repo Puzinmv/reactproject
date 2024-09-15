2b6608cce05227abc407a0adf53c0b97ea9e157e
Пузин Михаил <puzinmv@gmail.com>
2024-09-15 15:36:31 +0500

    изменения в хедере

--- src/Components/ResponsiveAppBar.js
+++ src/Components/ResponsiveAppBar.js
@@ -3,82 +3,136 @@ import {
     AppBar, Box, Toolbar, IconButton, Typography, Menu,
     Container, Avatar, Button, Tooltip, MenuItem
 } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const pages = ['Карта проекта'];
 const settings = ['Выйти'];
 
 function ResponsiveAppBar({ handleLogout, current }) {
-  //const [setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
 
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  //const handleCloseNavMenu = () => {
-  //  setAnchorElNav(null);
-  //};
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
     return (
         <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <img src="/logo.png" alt="Logo" style={{ marginRight: '16px', height: '40px' }} />
+            <Container maxWidth={false}>
+                <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+                    {/* Logo and Mobile menu */}
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }}>
+                            <img src="/logo.png" alt="Logo" style={{ height: '40px' }} />
+                        </Box>
+                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Box>
+
+                    {/* Mobile logo */}
+                    <Box sx={{ display: { xs: 'flex', md: 'none' }, flexGrow: 1, justifyContent: 'center' }}>
+                        <img src="/logo.png" alt="Logo" style={{ height: '40px' }} />
+                    </Box>
+
+                    {/* Desktop menu */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={() => { } }
+                                onClick={handleCloseNavMenu}
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                             >
                                 {page}
                             </Button>
                         ))}
                     </Box>
-                    {(Object.keys(current).length > 0) && (
-                        <Box sx={{ flexGrow: 0 }}>
+
+                    {/* User menu */}
+                    {Object.keys(current).length > 0 && (
+                        <Box>
                             <Tooltip title={`${current.first_name}`}>
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                                     <Avatar alt={`${current.first_name}`} src="" />
-                            </IconButton>
+                                </IconButton>
                             </Tooltip>
-                                <Menu
-                                    sx={{ mt: '45px' }}
-                                    id="menu-appbar"
-                                    anchorEl={anchorElUser}
-                                    anchorOrigin={{
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
                                     vertical: 'top',
                                     horizontal: 'right',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
+                                }}
+                                keepMounted
+                                transformOrigin={{
                                     vertical: 'top',
                                     horizontal: 'right',
-                                    }}
-                                    open={Boolean(anchorElUser)}
-                                    onClose={handleCloseUserMenu}
-                                >
-                                    {settings.map((setting) => (
-                                        <MenuItem key={setting}
-                                            onClick={() => {
-                                                handleCloseUserMenu();
-                                                handleLogout();
-                                            }}
-                                        >
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                    ))}
-                                </Menu>
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting}
+                                        onClick={() => {
+                                            handleCloseUserMenu();
+                                            handleLogout();
+                                        }}
+                                    >
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                                ))}
+                            </Menu>
                         </Box>
                     )}
                 </Toolbar>
             </Container>
         </AppBar>
-  );
+    );
 }
 export default ResponsiveAppBar;
